6c3749368a1b22d3f3b0d20158fa9e5257d5cbee
highlyko17 <highlyko17@gmail.com>
2023-12-05 10:57:19 +0900

    first commit

--- src/main/java/com/example/BoardController.java
+++ src/main/java/com/example/BoardController.java
@@ -1,7 +1,7 @@
 package com.example;
 
 
-import org.jetbrains.annotations.NotNull;
+//import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
